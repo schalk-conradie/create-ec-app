04316df50130475bddd97571512ae79f7ecc3920
Schalk Conradie <schalkuz.sc@gmail.com>
2026-01-28 06:46:53 +0200

    bug: fixed up issues with window.top on form based webresources

--- templates/targets/webresource/src/services/AuthService.ts
+++ templates/targets/webresource/src/services/AuthService.ts
@@ -1,38 +1,37 @@
-
 const getAuthToken = async (): Promise<string> => {
-    const tokenModule = await import("../../token.json");
-    const token = tokenModule.default.accessToken;
-    return token;
+	const tokenModule = await import("../../token.json");
+	const token = tokenModule.default.accessToken;
+	return token;
 };
 
 export const getApiUrl = (): string => {
-    if (window.parent && window.parent.Xrm) {
-        const globalContext = window.Xrm.Utility.getGlobalContext();
-        const clientUrl = globalContext.getClientUrl();
-        return `${clientUrl}/api/data/v9.2`;
-    }
+	if (window.top?.Xrm) {
+		const globalContext = window.top.Xrm.Utility.getGlobalContext();
+		const clientUrl = globalContext.getClientUrl();
+		return `${clientUrl}/api/data/v9.2`;
+	}
 
-    return "https://DOMAIN.REGION.dynamics.com/api/data/v9.2";
+	return "https://DOMAIN.REGION.dynamics.com/api/data/v9.2";
 };
 
 export const getAuthHeaders = async (): Promise<HeadersInit> => {
-    if (window.parent && window.parent.Xrm) {
-        return {
-            "Content-Type": "application/json",
-            "OData-MaxVersion": "4.0",
-            "OData-Version": "4.0",
-            Prefer: 'odata.include-annotations="*"',
-        };
-    }
+	if (window.top?.Xrm) {
+		return {
+			"Content-Type": "application/json",
+			"OData-MaxVersion": "4.0",
+			"OData-Version": "4.0",
+			Prefer: 'odata.include-annotations="*"',
+		};
+	}
 
-    const token = await getAuthToken();
-    const headers: HeadersInit = {
-        Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json",
-        "OData-MaxVersion": "4.0",
-        "OData-Version": "4.0",
-        Prefer: 'odata.include-annotations="*"',
-    };
+	const token = await getAuthToken();
+	const headers: HeadersInit = {
+		Authorization: `Bearer ${token}`,
+		"Content-Type": "application/json",
+		"OData-MaxVersion": "4.0",
+		"OData-Version": "4.0",
+		Prefer: 'odata.include-annotations="*"',
+	};
 
-    return headers;
+	return headers;
 };
